81254a0f79e25a7a3aefa0020956c2295b26032c
root <root@SFS-WIZARD.localdomain>
2020-11-03 17:25:48 -0800

    aubin();

--- CollaborativeProgram.java
+++ CollaborativeProgram.java
@@ -2,10 +2,15 @@ public class CollaborativeProgram {
    public static void main(String[] args){
       System.out.println("Hello world!");
       kai();
+      aubin();
    }
    
    public static void kai() {
       System.out.println("Hi Kai!");
+   }
+
+   public static void aubin() {
+      System.out.println("Hi Aubin!");
    }
    
    //add your own methods below (please make your name the name of your method)
