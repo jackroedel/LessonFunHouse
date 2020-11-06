06da5bb0640bd30b7a1e68877528ee2e50e4bd15
ewaite135 <ewaite135@gmail.com>
2020-11-05 16:49:19 -0800

    Adding my method

--- CollaborativeProgram.java
+++ CollaborativeProgram.java
@@ -2,10 +2,14 @@ public class CollaborativeProgram {
    public static void main(String[] args){
       System.out.println("Hello world!");
       kai();
+      eli();
    }
    
    public static void kai() {
       System.out.println("Hi Kai!");
+   }
+   public static void eli() {
+      System.out.println("Hi, this is Eli!");
    }
    
    //add your own methods below (please make your name the name of your method)
