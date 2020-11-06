78840dbb9ed0e21232c1aecd0603b2fd4f7f67ee
ManyiZ <zhaomanyi2005@gmail.com>
2020-11-05 16:52:34 -0800

    manyi's method

--- CollaborativeProgram.java
+++ CollaborativeProgram.java
@@ -2,6 +2,7 @@ public class CollaborativeProgram {
    public static void main(String[] args){
       System.out.println("Hello world!");
       kai();
+      manyi();
    }
    
    public static void kai() {
@@ -9,4 +10,7 @@ public class CollaborativeProgram {
    }
    
    //add your own methods below (please make your name the name of your method)
+   public static void manyi(){
+      System.out.println("Manyi was here");
+   }
 }
