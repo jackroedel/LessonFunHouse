0a456133d3cce056bddee789e0b5eb794e92dc39
eran4 <eranfann@gmail.com>
2020-11-05 16:51:30 -0800

    eranedited program

--- CollaborativeProgram.java
+++ CollaborativeProgram.java
@@ -2,11 +2,15 @@ public class CollaborativeProgram {
    public static void main(String[] args){
       System.out.println("Hello world!");
       kai();
+      eran();
    }
    
    public static void kai() {
       System.out.println("Hi Kai!");
    }
+   
+   public static void eran() {
+      System.out.println("Hi Eran..?");
    
    //add your own methods below (please make your name the name of your method)
 }
